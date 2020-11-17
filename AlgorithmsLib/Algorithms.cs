f4722a97e71ee36d88eee5ef737e381a71545a78
dimkson <dimkson@yahoo.com>
2020-11-17 20:45:39 +0300

    Добавьте файлы проекта.

---
+++ AlgorithmsLib/Algorithms.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLib
+{
+    public class Algorithms
+    {
+        /// <summary>
+        /// Алгоритм Евклида. Алгоритм нахождения наибольшего общего делителя (НОД) пары целых чисел вычитанием.
+        /// </summary>
+        public static int NOD(int a, int b)
+        {
+            //Алгоритм Эвклида
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+            return a;
+        }
+        /// <summary>
+        /// Алгоритм Евклида. Ускоренный алгоритм нахождения наибольшего общего делителя (НОД) пары целых чисел делением.
+        /// </summary>
+        public static int NODfast(int a, int b)
+        {
+            //Ускоренный алгоритм Эвклида
+            int c;
+            while (b != 0)
+            {
+                c = a % b;
+                a = b;
+                b = c;
+            }
+            return a;
+        }
+        /// <summary>
+        /// Алгоритм ускоренного возведения в степень.
+        /// </summary>
+        public static int Pow(int a, int b)
+        {
+            int n = 1, s = a, k = b;
+            while (k != 0)
+            {
+                if (k % 2 == 0)
+                {
+                    s *= s;
+                    k /= 2;
+                }
+                else
+                {
+                    n *= s;
+                    k--;
+                }
+            }
+            return n;
+        }
+    }
+}
