9f9410925ba33693e4df9377a5c77bd41d8165ed
dimkson <dimkson@yahoo.com>
2020-11-18 14:29:38 +0300

    Добавлена шейкерная сортировка.

--- AlgorithmsLib/Sort.cs
+++ AlgorithmsLib/Sort.cs
@@ -14,8 +14,8 @@ namespace AlgorithmsLib
         /// </summary>
         public static void Bubble(int[] array)
         {
-            int right = array.Length - 1;
-            int temp = 0;
+            int right = array.Length - 1,
+                temp = 0;
             bool flag = false;
             for (int i = 0; i < array.Length - 1; i++)
             {
@@ -30,6 +30,45 @@ namespace AlgorithmsLib
                 }
                 if (!flag) break;
                 right = temp;
+                flag = false;
+            }
+        }
+
+        /// <summary>
+        /// Шейкерная сортировка. Сложность алгоритма: O(n^2).
+        /// Алгоритм оптимизирован. Добавлены флаги для выхода, в случае если массив отсортирован. Внутренние циклы работают до места последнего обмена значений.
+        /// </summary>
+        public static void CoctailShaker(int[] array)
+        {
+            int left = 0,
+                right = array.Length - 1,
+                temp = 0;
+            bool flag = false;
+            while (left < right)
+            {
+                for (int i = left; i < right; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        flag = true;
+                        temp = i;
+                        Swap(ref array[i], ref array[i + 1]);
+                    }
+                }
+                if (!flag) break;
+                right = temp;
+                flag = false;
+                for (int i = right; i > left; i--)
+                {
+                    if (array[i] < array[i - 1])
+                    {
+                        flag = true;
+                        temp = i;
+                        Swap(ref array[i], ref array[i - 1]);
+                    }
+                }
+                if (!flag) break;
+                left = temp;
                 flag = false;
             }
         }
