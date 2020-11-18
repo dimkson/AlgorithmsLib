f711524206d2a4f92896d85af2edca6e89209742
dimkson <dimkson@yahoo.com>
2020-11-18 13:50:21 +0300

    Создан класс Sort. Добавлена сортировка пузырьком.

---
+++ AlgorithmsLib/Sort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLib
+{
+    static public class Sort
+    {
+        /// <summary>
+        /// Сортировка пузырьком. Сложность алгоритма: O(n^2).
+        /// Алгоритм оптимизирован. Добавлен флаг для выхода, в случае если массив отсортирован. Внутренний цикл работает до места последнего обмена значений.
+        /// </summary>
+        public static void Bubble(int[] array)
+        {
+            int right = array.Length - 1;
+            int temp = 0;
+            bool flag = false;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < right; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        flag = true;
+                        temp = j;
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+                if (!flag) break;
+                right = temp;
+                flag = false;
+            }
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+        }
+    }
+}
